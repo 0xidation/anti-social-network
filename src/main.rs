ac0012e2cf4e9fa524cef53847daa570f240078f
Luke Osborne <lukewosborne@gmail.com>
2018-07-30 20:28:41 -0400

    add basic routes with form validation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,75 @@
-#![feature(plugin)]
+#![feature(plugin, custom_derive)]
 #![plugin(rocket_codegen)]
-#[cfg(test)] mod tests;
 
 extern crate rocket;
-use rocket::Rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "The Anti-Social Network"
+#[cfg(test)] mod tests;
+
+use rocket::response::NamedFile;
+use rocket::response::Redirect;
+use rocket::request::{Form, FromFormValue};
+use rocket::http::RawStr;
+use std::io;
+
+#[derive(Debug)]
+struct StrongPassword<'r>(&'r str);
+
+#[derive(FromForm)]
+struct UserLogin<'r> {
+    username: &'r RawStr,
+    password: Result<StrongPassword<'r>, &'static str>,
 }
 
-fn rocket() -> Rocket {
-    rocket::ignite().mount("/", routes![index])
+impl<'v> FromFormValue<'v> for StrongPassword<'v> {
+    type Error = &'static str;
+
+    fn from_form_value(v: &'v RawStr) -> Result<Self, Self::Error> {
+        if v.len() < 10 {
+            Err("Password must be at least 10 characters.")
+        } else {
+            Ok(StrongPassword(v.as_str()))
+        }
+    }
+}
+
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
+
+#[get("/login")]
+fn login_page() -> io::Result<NamedFile> {
+    NamedFile::open("static/login.html")
+}
+
+#[post("/login", data = "<user_form>")]
+fn login<'a>(user_form: Form<'a, UserLogin<'a>>) -> Result<Redirect, String> {
+    let user = user_form.get();
+
+    if let Err(e) = user.password {
+        return Err("Incorrect Username or Password".to_string());
+    }
+
+    // TODO: use real database for this later.
+    if user.username == "Luke" {
+        if let Ok(StrongPassword("password01")) = user.password {
+            Ok(Redirect::to("/user/Luke"))
+        } else {
+            Err("Incorrect Username or Password".to_string())
+        }
+    } else {
+        Err("Incorrect Username or Password".to_string())
+    }
+}
+
+#[get("/user/<username>")]
+fn user_page(username: &RawStr) -> String {
+    format!("This is {}'s page.", username)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, user_page, login_page, login])
 }
 
 fn main() {
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,79 @@
 use super::rocket;
 use rocket::local::Client;
-use rocket::http::Status;
+use rocket::http::{ContentType, Status};
 
 #[test]
-fn index_test() {
+fn test_index() {
     let client = Client::new(rocket()).expect("valid rocket instance");
     let mut response = client.get("/").dispatch();
     assert_eq!(response.status(), Status::Ok);
-    assert_eq!(response.body_string(), Some("The Anti-Social Network".into()));
+    assert!(response.body_string().is_some());
+}
+
+fn test_login<T>(user: &str, pass: &str, status: Status, body: T)
+    where T: Into<Option<&'static str>>
+{
+    let client = Client::new(rocket()).unwrap();
+    let query = format!("username={}&password={}", user, pass);
+    let mut response = client.post("/login")
+        .header(ContentType::Form)
+        .body(&query)
+        .dispatch();
+
+    assert_eq!(response.status(), status);
+    if let Some(expected_str) = body.into() {
+        let body_str = response.body_string();
+        assert!(body_str.map_or(false, |s| s.contains(expected_str)));
+    }
+}
+
+#[test]
+fn test_good_login() {
+    test_login("Luke", "password10", Status::Ok, None);
+}
+
+#[test]
+fn test_invalid_user() {
+    test_login("-1", "password10", Status::Ok, "Incorrect Username or Password");
+    test_login("Mike", "password10", Status::Ok, "Incorrect Username or Password");
+}
+
+#[test]
+fn test_invalid_password() {
+    test_login("Luke", "password101", Status::Ok, "Incorrect Username or Password");
+    test_login("Luke", "ok", Status::Ok, "Incorrect Username or Password");
+}
+
+fn check_bad_form(form_str: &str, status: Status) {
+    let client = Client::new(rocket()).unwrap();
+    let response = client.post("/login")
+        .header(ContentType::Form)
+        .body(form_str)
+        .dispatch();
+
+    assert_eq!(response.status(), status);
+}
+
+#[test]
+fn test_bad_form_abnromal_inputs() {
+    check_bad_form("&&&===&", Status::BadRequest);
+    check_bad_form("&&&=hi==&", Status::BadRequest);
+}
+
+#[test]
+fn test_bad_form_missing_fields() {
+    let bad_inputs: [&str; 2] = [
+        "&",
+        "=",
+    ];
+
+    for bad_input in bad_inputs.into_iter() {
+        check_bad_form(bad_input, Status::BadRequest);
+    }
+}
+
+#[test]
+fn test_bad_form_additional_fields() {
+    check_bad_form("username=Luke&password=pass&addition=1",
+                   Status::UnprocessableEntity);
 }
